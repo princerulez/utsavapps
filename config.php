e94aa9b93479f991825992f5329fc465572edf54
princerulez <coolutsav999@gmail.com>
2013-05-08 12:47:21 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+// šđžčć - keep utf-8 encoding
+/**
+* Femina Portal - Love Calculator Facebook application
+* Config file with constants needed for Facebook app
+* 
+* LICENSE: This source file is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License 3
+* as published by the Free Software Foundation.
+*
+* @author     INFORBIRO - Information Technology and Marketing Agency (www.inforbiro.com) <office@inforbiro.com>
+* @copyright  Femina Portal - Female Internet Portal (www.FeminaPortal.com) <info@feminaportal.com>
+* @license    GNU General Public License 3, see gpl_license_v3.0.txt
+* @link       http://www.inforbiro.com; http://www.feminaportal.com
+* @demo       http://apps.facebook.com/fp-love-calculator/
+* @source       http://www.inforbiro.com/blog-eng/free-viral-facebook-application-with-step-by-step-tutorial/
+*/
+
+define("FACEBOOK_APP_ID", '  313523565447909');
+define("FACEBOOK_API_KEY", 'ab21a1be4cf5af182692a6edc4e43545ab21a1be4cf5af182692a6edc4e43545');
+define("FACEBOOK_SECRET_KEY", 'ab21a1be4cf5af182692a6edc4e43545');
+define("FACEBOOK_CANVAS_URL", 'http://apps.facebook.com/herokudev');
+define("FACEBOOK_DOMAIN", 'http://ancient-lowlands-1062.herokuapp.com/');
+
+
+?>
